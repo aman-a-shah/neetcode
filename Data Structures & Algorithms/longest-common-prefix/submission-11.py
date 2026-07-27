335d0e927f73cd98bec6c26db686fc0ffabcae6a
Aman Shah <233501241+aman-a-shah@users.noreply.github.com>
2026-07-27 09:33:38 -0400

    Add: longest-common-prefix - submission-11

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-11.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        common = strs[0]
+
+        for wrd in strs:
+            if len(wrd) < len(common):
+                common = common[:len(wrd)]
+            for i, let in enumerate(wrd):
+                if i >= len(common) or common[i] != wrd[i]:
+                    common = wrd[:i]
+                    break
+                
+        return common
